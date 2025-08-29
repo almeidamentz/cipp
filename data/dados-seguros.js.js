1f2f74008efc4ea78d9218a70dec770f4599ea05
almeidamentz <151780180+almeidamentz@users.noreply.github.com>
2025-08-29 08:57:25 -0400

    Update dados-seguros.js.js

--- data/dados-seguros.js.js
+++ data/dados-seguros.js.js
@@ -14,7 +14,7 @@ const dadosInscricoes = [
     {"datainscriçăo":"18/08/2025","Endereço de e-mail":"janemeira.pereira@gmail.com","Tipo da Inscriçăo":"Sem Acompanhante","Nome Completo":"Jane meira","Gęnero":"Feminino","Cidade":"Cuiabá","Idade":"53","status":"confirmado"},
     {"datainscriçăo":"18/08/2025","Endereço de e-mail":"molinarilucass11@gmail.com","Tipo da Inscriçăo":"Sem Acompanhante","Nome Completo":"Lucas Molinari leal","Gęnero":"Masculino","Cidade":"Cuiabá","Idade":"30","status":"confirmado"},
     {"datainscriçăo":"18/08/2025","Endereço de e-mail":"moniquekar@gmail.com","Tipo da Inscriçăo":"Sem Acompanhante","Nome Completo":"Sidnei Luiz de Souza Bueno","Gęnero":"Masculino","Cidade":"Cuiabá","Idade":"43","status":"confirmado"},
-    {"datainscrição":"18/08/2025","Endereço de e-mail":"lucykarola@gmail.com","Tipo da Inscrição":"Sem Acompanhante","Nome Completo":"Lucy lima","Gênero":"Feminino","Cidade":"Cuiabá","Idade":"59","status":"confirmado"},
+  
 
     // Adicione aqui TODOS os registros do CSV...
 ];
